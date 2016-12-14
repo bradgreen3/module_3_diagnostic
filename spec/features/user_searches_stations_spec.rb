7eb55cc8afdde02d12d0ecfe8ef37d407dd4a19d
Brad <bgreen3@uoregon.edu>
2016-12-14 09:49:28 -0700

    First feature test almost complete

--- spec/features/user_searches_stations_spec.rb
+++ spec/features/user_searches_stations_spec.rb
@@ -5,10 +5,11 @@ describe "user visits root page" do
   context "and fills in form" do
     it "they are redirected to the search page" do
       visit '/'
-      fill_in('Zip', :with => '80203')
+      fill_in("q", :with => '80203')
       click_on 'Locate'
 
       expect(current_path).to eq('/search')
+      expect(current_url).should have_content("80203")
     end
   end
 end
